7e85f5b4faf5ca5dc2dbc20a03b0172c95b5daf0
iampengjun <92166909+iampengjun@users.noreply.github.com>
2021-10-12 17:48:33 +0200

    Create caesar.py

---
+++ caesar.py
@@ -0,0 +1,17 @@
+def caesar(text, n):
+    for x in text:
+        if ord('a') <= ord(x) <= ord('z'):
+            if ord(x) + n >= ord('z'):
+                print(chr(ord(x) + n - 26), end='')
+            else:
+                print(chr(ord(x) + n), end='')
+        elif ord('A') <= ord(x) <= ord('Z'):
+            if ord(x) + n >= ord('Z'):
+                print(chr(ord(x) + n - 26), end='')
+            else:
+                print(chr(ord(x) + n), end='')
+        else:
+            print(x, end='')
+
+
+caesar("Hello, world!", 5)
